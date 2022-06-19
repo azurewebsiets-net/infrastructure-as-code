599d09f7fbf37aabfef6e677bffa77ed4765148b
TeamDman <TeamDman9201@gmail.com>
2022-06-18 21:55:11 -0400

    move to subfolder

--- ca-teamdman/dns.tf
+++ ca-teamdman/dns.tf
@@ -20,3 +20,11 @@ resource "azurerm_dns_a_record" "root" {
   ttl                 = 30
   target_resource_id  = azurerm_static_site.main.id
 }
+
+resource "azurerm_dns_a_record" "wildcard" {
+  resource_group_name = azurerm_resource_group.main.name
+  zone_name           = azurerm_dns_zone.main.name
+  name                = "*"
+  ttl                 = 30
+  target_resource_id  = azurerm_static_site.main.id
+}
