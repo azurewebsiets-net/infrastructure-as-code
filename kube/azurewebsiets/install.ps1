26adff35988937c3ab9e7573e5a3d587fb277b56
TeamDman <TeamDman9201@gmail.com>
2022-06-27 01:59:05 -0400

    TLS working finally

--- kube/azurewebsiets/install.ps1
+++ kube/azurewebsiets/install.ps1
@@ -1,8 +1,16 @@
+Write-Host "Gathering Azure resource information" -ForegroundColor Cyan
+
 $rg = "net.azurewebsiets";
-$MIClientId=$(az aks show --name shared-cluster --resource-group shared-cluster --query identityProfile.kubeletidentity.clientId -o tsv)
+$MIClientId = $(az aks show --name shared-cluster --resource-group shared-cluster --query identityProfile.kubeletidentity.clientId -o tsv)
 $acc = az account show --output json | ConvertFrom-Json;
 $tenantid = $acc.tenantId;
 $subscriptionId = $acc.id;
+
+$keys = az storage account keys list `
+    --resource-group "net.azurewebsiets" --account-name "azurewebsiets" `
+| ConvertFrom-Json;
+
+Write-Host "Applying Helm config" -ForegroundColor Cyan
 
 helm upgrade `
     --install `
@@ -12,5 +20,6 @@ helm upgrade `
     --set "rg=$rg" `
     --set "dns=azurewebsiets.net" `
     --set "clientId=$MIClientId" `
+    --set "storageAccountKey=`"$($keys[0].value)`"" `
     "azurewebsiets" `
     "./"
